eb26716f95096aa58c9441145630a5f8799a30fa
Hemanth2709 <geniushemu2000@gmail.com>
2025-05-24 19:30:30 +0530

    FIX : VIte changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import "./App.css";
-import { live } from '@electric-sql/pglite/live';
+import { live } from "@electric-sql/pglite/live";
 import { PGliteProvider } from "@electric-sql/pglite-react";
 import PatientRegistrationApp from "./components/PatientRegistrationApp";
 import { PGliteWorker } from "@electric-sql/pglite/worker";
-import type { PGliteWithLive } from '@electric-sql/pglite/live';
+import type { PGliteWithLive } from "@electric-sql/pglite/live";
 import { useEffect } from "react";
+import PGWorker from "./pglite-worker.js?worker";
 
 const db = new PGliteWorker(
-  new Worker(new URL('./pglite-worker.js', import.meta.url), {
-    type: 'module',
+  new PGWorker({
+    name: "pglite-worker", // OK
   }),
   {
-    dataDir: 'opfs-ahp://patient-registration-db',
+    dataDir: "opfs-ahp://patient-registration-db",
     extensions: {
-      live
-    }
+      live,
+    },
   }
 ) as unknown as PGliteWithLive;
 
 function App() {
   useEffect(() => {
-    db.exec(`
+    db.exec(
+      `
       CREATE TABLE IF NOT EXISTS patients (
         id SERIAL PRIMARY KEY,
         first_name VARCHAR(100) NOT NULL,
@@ -32,9 +34,12 @@ function App() {
         address TEXT,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
-    `).then(res => {
-      console.log(res);
-    }).catch(err => console.log(err));
+    `
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
